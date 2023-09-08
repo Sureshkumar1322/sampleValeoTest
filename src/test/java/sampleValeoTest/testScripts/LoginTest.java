391ad359ded7b11d7b16336c72af8b06d5c163fc
Sureshkumar1322 <sureshkumark14797@gmail.com>
2023-09-08 15:29:44 +0530

    initial project

---
+++ src/test/java/sampleValeoTest/testScripts/LoginTest.java
@@ -0,0 +1,23 @@
+package sampleValeoTest.testScripts;
+
+import static org.testng.Assert.assertTrue;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+  @Test
+  public void validLogin() {
+	  WebDriver driver = new ChromeDriver();
+	  driver.manage().window().maximize();
+	  driver.get("https://the-internet.herokuapp.com/login");
+	  driver.findElement(By.id("username")).sendKeys("tomsmith");
+	  driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+	  driver.findElement(By.className("radius")).click();
+	  boolean displayed = driver.findElement(By.id("flash")).isDisplayed();
+	  assertTrue(displayed);
+  }
+}
